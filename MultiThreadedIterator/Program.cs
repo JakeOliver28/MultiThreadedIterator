b6276b49a228732945c397f919ad2403c244ce65
Jake Oliver <jeoliver97@gmail.com>
2025-03-29 10:48:50 -0400

    Add project files.

---
+++ MultiThreadedIterator/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Threading;
+using System.Collections.Concurrent;
+
+Console.WriteLine("Enter directory paths to be iterated.");
